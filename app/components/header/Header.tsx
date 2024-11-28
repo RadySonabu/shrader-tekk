7e15a20f9f0ab32faa0d36c98076d2fbf7619829
Jury Gregorio Jr <jury.devs@gmail.com>
2024-11-28 22:23:18 +0800

    added bg header

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,20 +1,21 @@
 import "./header.css";
 import Link from "next/link";
 import { FaFacebookSquare } from "react-icons/fa";
-import Image from "next/image";
+// import Image from "next/image";
 import { FaPlay, FaSquareYoutube } from "react-icons/fa6";
 import { AiFillTikTok } from "react-icons/ai";
+import VideoBackground from "./videoBG/videoBackground";
 
 export default function Header() {
-  const imgStyle = {
-    opacity: 0.2,
-  };
+  // const imgStyle = {
+  //   opacity: 0.2,
+  // };
 
   return (
     <header className="w-full header z-10">
       <div className="hero-background -z-1">
         {/* *** Background *** */}
-        <div className="circle">
+        {/* <div className="circle">
           <Image
             src="/images/circle-transparent.png"
             alt="Circle Background"
@@ -22,7 +23,8 @@ export default function Header() {
             height={100}
             style={imgStyle}
           />
-        </div>
+        </div> */}
+        <VideoBackground />
 
         {/* *** End Background *** */}
         {/* *** Content *** */}
@@ -35,6 +37,12 @@ export default function Header() {
 
           <div className="elevate-description font-[family-name:var(--poppins)]">
             <div className="elevate-videos">
+              <p className="font-[family-name:var(--monda)] tracking-wide font-medium">
+                Let us skyrocket your social media.  Let us build your apps. 
+                Let us plan and build your solutions with greater efficiency,
+                <span> affordability and flexibility </span>than you will find
+                anywhere else.
+              </p>
               <div className="videos">
                 <iframe
                   width="560"
@@ -47,37 +55,30 @@ export default function Header() {
                   allowFullScreen
                 ></iframe>
               </div>
+            </div>
+          </div>
+          <div className="header-links z-10 pb-4">
+            <div className="social-header-links">
+              <Link href="https://www.facebook.com/shradertech/">
+                <FaFacebookSquare className="h-8 w-8 text-black-600 hover:text-blue-600 transition duration-300" />
+              </Link>
+              <Link href="https://youtube.com/@shradertechnologies">
+                <FaSquareYoutube className="h-8 w-8 hover:text-red-600 transition duration-300" />
+              </Link>
 
-              <p>
-                Let us skyrocket your social media.  Let us build your apps. 
-                Let us plan and build your solutions with greater efficiency,
-                <span> affordability and flexibility </span>than you will find
-                anywhere else.
-              </p>
+              <Link href="https://www.tiktok.com/@shrader.tech">
+                <AiFillTikTok className="h-8 w-8 hover:text-teal-600 transition duration-300" />
+              </Link>
+            </div>
+            <div className="learn-header-links py-3 px-6 font-[family-name:var(--poppins)]">
+              <Link href="/contact" className="flex items-center gap-2">
+                Learn More
+                <FaPlay className="h-6 w-6 faplay-icons text-blue-600" />
+              </Link>
             </div>
           </div>
         </div>
         {/* *** End Content *** */}
-        <div className="header-links z-10 pb-4">
-          <div className="social-header-links">
-            <Link href="https://www.facebook.com/shradertech/">
-              <FaFacebookSquare className="h-8 w-8 text-black-600 hover:text-blue-600 transition duration-300" />
-            </Link>
-            <Link href="hhttps://youtube.com/@shradertechnologies">
-              <FaSquareYoutube className="h-8 w-8 hover:text-red-600 transition duration-300" />
-            </Link>
-
-            <Link href="https://www.tiktok.com/@shrader.tech">
-              <AiFillTikTok className="h-8 w-8 hover:text-teal-600 transition duration-300" />
-            </Link>
-          </div>
-          <div className="learn-header-links py-3 px-6 font-[family-name:var(--poppins)]">
-            <Link href="/contact" className="flex items-center gap-2">
-              Learn More
-              <FaPlay className="h-6 w-6 faplay-icons" />
-            </Link>
-          </div>
-        </div>
       </div>
     </header>
   );
--- app/components/header/videoBG/videoBackground.tsx
+++ app/components/header/videoBG/videoBackground.tsx
@@ -2,17 +2,7 @@ import React from "react";
 import styles from "./VideoBackground.module.css"; // Importing the styles
 
 const VideoBackground = () => {
-  return (
-    <div className={styles.videoBackground}>
-      <video autoPlay muted loop className={styles.backgroundVideo}>
-        <source
-          src="/videos/background/headerbackground.mp4"
-          type="video/mp4"
-        />
-        Your browser does not support the video tag.
-      </video>
-    </div>
-  );
+  return <div className={styles.videoBackground}></div>;
 };
 
 export default VideoBackground;
